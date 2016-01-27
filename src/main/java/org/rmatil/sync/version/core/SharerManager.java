cd9fd5db085f8d5d418b34f4eec94db0e11d172b
Raphael Matile <raphael.matile@gmail.com>
2016-01-27 20:28:02 +0100

    Remove obsolete import statement

--- src/main/java/org/rmatil/sync/version/core/SharerManager.java
+++ src/main/java/org/rmatil/sync/version/core/SharerManager.java
@@ -12,7 +12,6 @@ import org.rmatil.sync.version.core.model.Sharer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
-import java.util.UUID;
 
 public class SharerManager implements ISharerManager {
 
