ccd8cf753927ef50d55747b56eb12781f210ea87
Raphael Matile <raphael.matile@gmail.com>
2016-02-02 09:33:44 +0100

    Reorder access type enum

--- src/main/java/org/rmatil/sync/version/api/AccessType.java
+++ src/main/java/org/rmatil/sync/version/api/AccessType.java
@@ -1,10 +1,30 @@
 package org.rmatil.sync.version.api;
 
+/**
+ * The access types for a sharer.
+ * <p>
+ * Note, that the order of the values
+ * is important, since they build an implicit
+ * chain of access: Every sharer having write access,
+ * has also access to read. However, a sharer having only read
+ * access does not have write access.
+ * <code>Enum.values()</code> can be used to get the values
+ * in declared order
+ */
 public enum AccessType {
 
-    WRITE,
+    /**
+     * If a sharer lost his read/write access
+     */
+    ACCESS_REMOVED,
 
+    /**
+     * If a sharer has read access
+     */
     READ,
 
-    ACCESS_REMOVED
+    /**
+     * If a sharer should have write access
+     */
+    WRITE
 }
